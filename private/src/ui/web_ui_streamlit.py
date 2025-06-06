0b7220039bf6338782752e9d4450fe104d3aab66
noa <noa@n0v0.de>
2025-06-06 10:46:14 +0200

    Add caching to reduce response time

--- private/src/ui/web_ui_streamlit.py
+++ private/src/ui/web_ui_streamlit.py
@@ -1,12 +1,17 @@
 import streamlit as st
-from PIL import Image, ImageFile
+from PIL import Image
 import plotly.express as px
 import numpy as np
 from ultralytics import YOLO
 
-model_name = "yolo11m400"
+model_name = "yolov8l400"
 model_path = f"../../generated/{model_name}/weights/best.pt"
-model = YOLO(model_path)
+
+@st.experimental_singleton
+def load_model(model_path):
+    return YOLO(model_path)
+
+model = load_model(model_path)
 
 def main():
     st.title("bone fracture detection")
@@ -15,15 +20,28 @@ def main():
 
     annotation_file = st.file_uploader(label="Upload annotation (optional)", type=["txt"])
 
-    threshold = st.slider(label="Confidence threshold", min_value=0.001, max_value=0.5, value=0.05, step=0.001, format="%0.3f")
+    threshold = st.slider(label="", min_value=0.001, max_value=0.5, value=0.1, step=0.001, format="%0.3f")
 
     image_results = []
 
-    for i in range(len(images)):
-        images[i] = Image.open(images[i])
-        images[i] = images[i].convert("RGBA")
 
-        result = model.predict(images[i], conf=0.001)[0].summary()
+    # create image placeholders
+    px_image_figures = []
+    st_image_elements = []
+    # st_text_elements = []
+    for _ in range(len(images)):
+        # st_text_elements.append(st.empty())
+        st_image_elements.append(st.empty())
+
+    for i in range(len(images)):
+        images[i] = open_image(images[i])
+
+        fig = imshow(images[i])
+        px_image_figures.append(fig)
+        st_image_elements[i].plotly_chart(fig)
+
+    for i in range(len(images)):
+        result = predict(images[i])
 
         new_result = []
         for i, r in enumerate(result):
@@ -34,20 +52,30 @@ def main():
         image_results.append(result)
 
     for i in range(len(images)):
-        fig = px.imshow(images[i])
+        fig = px_image_figures[i]
 
         if annotation_file is not None:
             img_size_x, img_size_y = images[i].size
             add_annotation_file_boxes(fig, annotation_file, img_size_x, img_size_y)
 
         if len(image_results[i]) > 0:
-            "Bruch gefunden"
             add_model_prediction_boxes(fig, image_results[i])
-        else:
-            "kein Bruch gefunden"
 
-        st.plotly_chart(fig, use_container_width=True)
+        st_image_elements[i].plotly_chart(fig)
 
+@st.experimental_memo
+def open_image(image):
+    image = Image.open(image)
+    image = image.convert("RGBA")
+    return image
+
+# @st.experimental_memo
+def imshow(_image):
+    return px.imshow(_image)
+
+# @st.experimental_memo
+def predict(_image):
+    return model.predict(_image, conf=0.001)[0].summary()
 
 def add_annotation_file_boxes(fig, annotation_file, img_size_x, img_size_y):
     annotation = annotation_file.read().decode("utf-8")
@@ -56,7 +84,6 @@ def add_annotation_file_boxes(fig, annotation_file, img_size_x, img_size_y):
     for b in boxes:
         x0, y0, x1, y1 = b[0] * img_size_x, b[1] * img_size_y, b[2] * img_size_x, b[3] * img_size_y
         fig.add_shape(x0=x0, y0=y0, x1=x1, y1=y1, line={"color": "rgba(0, 0, 255, 0.4)"})
-
 
 def add_model_prediction_boxes(fig, result):
     for rect in result:
